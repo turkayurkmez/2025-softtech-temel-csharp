c7e1427a3d9bb2c82f247157d4e791ec709dc93d
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-20 15:10:32 +0300

    Add .NET solution and console application for traffic lights
    This commit introduces a new Visual Studio solution file (`ConditionSwitch.sln`) and a project file (`ConditionSwitch.csproj`) for a .NET application. The project is configured with the necessary settings, including SDK and target framework. A new `Program.cs` file is added, implementing a console application that prompts the user to select a traffic light color and responds with appropriate messages using `if-else` and `switch` statements. The program also includes logic to determine and display whether the current day is a weekday or weekend.

---
+++ ConditionSwitch/ConditionSwitch/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hangi ışık yanıyor?\n1. Kırmızı\n2. Sarı\n3. Yeşil");
+Console.WriteLine("Lütfen seçiminizi sayı olarak giriniz");
+string choose = Console.ReadLine();
+
+if (choose == "1")
+{
+    Console.WriteLine("DUR!");
+}
+else if (choose == "2")
+{
+    Console.WriteLine("DİKKAT!!!");
+}
+else if (choose == "3")
+{
+    Console.WriteLine("GEÇ");
+}
+else
+{
+    Console.WriteLine("Hatalı seçim....");
+}
+
+switch (choose)
+{
+    case "1":
+        Console.WriteLine("DUR!");
+        break;
+    case "2":
+        Console.WriteLine("DİKKAT!!!");
+        break;
+    case "3":
+        Console.WriteLine("GEÇ");
+        break;
+
+    default:
+        Console.WriteLine("Hatalı seçim....");
+        break;
+}
+
+switch (DateTime.Now.DayOfWeek)
+{
+    case DayOfWeek.Monday:
+    case DayOfWeek.Tuesday:
+    case DayOfWeek.Wednesday:
+    case DayOfWeek.Thursday:
+    case DayOfWeek.Friday:
+        Console.WriteLine("Hafta içi....");
+        break;
+    case DayOfWeek.Sunday:
+    case DayOfWeek.Saturday:
+        Console.WriteLine("Hafta sonu");
+        break;
+    default:
+        break;
+}

2f66972fb46c14871d6e167c2c88d7022e6d7cd2
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-20 14:33:21 +0300

    Add .NET 9.0 project and initial console application
    Created a new Visual Studio solution (`ConditionIF.sln`) and project file (`ConditionIF.csproj`) targeting .NET 9.0. The project is set up as an executable with implicit usings and nullable reference types enabled. Added `Program.cs` with a simple console application demonstrating conditional statements, including even/odd checks and grading logic, with comments in Turkish for clarity.

---
+++ ConditionIF/ConditionIF/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int x = 5;
+if (x % 2 == 0)
+{
+    Console.WriteLine("Çifttir");
+}
+else
+{
+    Console.WriteLine("Tektir");
+}
+
+int score = 75;
+
+//&& -> And also
+//|| -> Or Else
+
+if (score >= 85 && score <=100)
+{
+    Console.WriteLine("AA");
+}
+else if (score >= 75 && score <=84)
+{
+    Console.WriteLine("AB");
+}
+else
+{
+    Console.WriteLine("Yukarıdakilerden hiçbiri");
+}
+
+//VE -> HEM .... HEM DE...
+//VEYA -> Ya .... Ya da....
+string output = string.Empty;
+int y = 5;
+if (y % 2 == 0)
+{
+    output = "Çifttir"; 
+}
+else
+{
+    output = "Tektir";
+}
+
+Console.WriteLine(output);
+//ternary operator:
+string sameOutput = y % 2 == 0 ? "Çift" : "Tek";
+
+Console.WriteLine(sameOutput);
+

2817cd997bb7f22df580dc609ca02c7cb7ad48fb
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-20 12:20:57 +0300

    Add age calculation program and new CustomOp class
    Implemented a program in `Program.cs` that prompts the user for their birth year, calculates their age, and handles overflow exceptions for byte summation. Introduced various data types including `byte`, `sbyte`, `short`, `ushort`, `int`, `long`, `double`, `float`, `decimal`, `char`, and `string`. Created a new `CustomOp.cs` file with a namespace and an empty class definition.

--- variablesAndTypes/variablesAndTypes/Program.cs
+++ variablesAndTypes/variablesAndTypes/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace variablesAndTypes
+﻿//
+Console.WriteLine("Hello, World!");
+int currentYear =  DateTime.Now.Year;
+Console.WriteLine("Lütfen doğum yılınızı giriniz:");
+string? answer = Console.ReadLine();
+int birthDate = Convert.ToInt32(answer);
+int age = currentYear - birthDate;
+Console.WriteLine("yaşınız " + age);
+
+/*
+ * sayısal
+ *   tam sayılar
+ *   ondalıklı sayılar
+ * sözel
+ * mantıksal
+ */
+
+byte eightBit = 255;
+sbyte signedEightBit = 127;
+
+Console.WriteLine("Bir sayı giriniz:");
+byte x = byte.Parse(Console.ReadLine());
+Console.WriteLine("Bir sayı daha giriniz:");
+
+byte y = Convert.ToByte(Console.ReadLine());
+
+try
 {
-    internal class Program
+    checked
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        byte total = (byte)(x + y);
+        Console.WriteLine($"{x} ve {y} byte değerlerinin toplamı {total}");
     }
 }
+catch(OverflowException ) 
+{
+    Console.WriteLine("Toplam değer, byte tipine atanamıyor. Lütfen toplam en fazla 255 olacak biçimde sayılar giriniz");
+}
+
+
+short sixteenBit = 32767;
+ushort unSignedSixteenBit = 65535;
+
+int thirhtyTwo = 0;
+//uint
+long veryBig = 0;
+//ulong 
+
+double pi = 3.14d;
+float fPi = 3.14f;
+decimal decPi = 3.14m;
+
+char symbol = '!';
+string multipleChar = "Selam!";
+
+bool isColored = false;
+

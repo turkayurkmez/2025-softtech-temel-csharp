feaea7c5b06ff510fab9cbb9949cb0617779ed79
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-20 13:56:52 +0300

    Add console app with exception handling and project files
    Created a new Visual Studio solution (`usingTryCatch.sln`) and project file (`usingTryCatch.csproj`) for a console application. The application prompts the user for two integers, performs division, and includes exception handling for `FormatException`, `DivideByZeroException`, and a general exception. The project targets .NET 9.0 with implicit usings and nullable reference types enabled.

---
+++ usingTryCatch/usingTryCatch/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+	Console.WriteLine("Bölünecek sayıyı girin");
+	int number = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine("Bölen sayıyı girin:");
+	int divider = Convert.ToInt32(Console.ReadLine());
+
+	int result = number / divider;
+
+	Console.WriteLine($"Bölüm: {result}");
+}
+catch (FormatException)
+{
+
+    Console.WriteLine("Lütfen yalnızca sayı giriniz");
+}
+catch (DivideByZeroException)
+{
+	Console.WriteLine("Tam sayılar 0'a bölünemez");
+	return;
+
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+
+}
+finally//ne olursa olsun (hata alsan da almasan da) çalış:
+{
+    Console.WriteLine("Çalışır mı?");
+}
+
+
+
+
+//1. FormatException
+//2. DivideByZeroException
+

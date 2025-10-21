330c7eaa7ddcbe033cf35db7737ab388ce0c4045
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-21 11:44:32 +0300

    Add initial project structure and console application
    Created a new Visual Studio solution and project files for a .NET 9.0 application. Added a console application in `Program.cs` that prints "Hello, World!" and demonstrates string manipulation techniques, including substring checks and rune enumeration.

---
+++ BuiltInFunctions/BuiltInFunctions/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string word = "adana";
+if (word.EndsWith("tech"))
+{
+    Console.WriteLine("tech ile biter");
+}
+
+var runes = word.EnumerateRunes();
+foreach (var item in runes)
+{
+    Console.WriteLine($"Plane: { item.Plane }\nValue: {item.Value} {(char)item.Value}\nUTF8Length   {item.Utf8SequenceLength}\nIs Ascii: {item.IsAscii}");
+}
+
+int startIndex = 0;
+var findingIndex = word.IndexOf('a',startIndex);
+Console.WriteLine($"p harfinin indexi: {findingIndex}");
+
+Console.WriteLine($"{word.IndexOf('a',0)}");
+Console.WriteLine($"{word.IndexOf('a', 1)}");
+Console.WriteLine($"{word.IndexOf('a', 3)}");
+Console.WriteLine($"{word.IndexOf('a', 5)}");
+
+while (findingIndex != -1)
+{
+   
+    startIndex = findingIndex +1;
+    Console.Write(findingIndex);
+    findingIndex = word.IndexOf('a', startIndex);
+}
+Console.WriteLine();

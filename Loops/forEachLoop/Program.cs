acb1ed120305653b1f8d84d39737309b6c8e2244
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-21 10:34:43 +0300

    Add forLoop and forEachLoop projects to solution
    Updated the `Loops.sln` file to include two new projects: `forLoop` and `forEachLoop`. Created project files with .NET SDK targeting `net9.0`, enabling implicit usings and nullable reference types.
    
    Added `Program.cs` in `forEachLoop` demonstrating boxing/unboxing with `ArrayList` and iterating with `foreach`. In `forLoop`, implemented a `for` loop to check for negative numbers in an array and print the result.

---
+++ Loops/forEachLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+string[] words = { "Ayna","Kelime" };
+
+ArrayList arrayList = new ArrayList();
+arrayList.Add("Türkay");//boxing
+arrayList.Add(45);
+arrayList.Add(true);
+
+int value = (int)arrayList[0]; //unboxing
+
+List<string> names = new List<string>();
+names.Add("Selçuk");
+names.Add("Aysun");
+
+foreach (var item in names)
+{
+    
+}
+
---
+++ Loops/forLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var numbers = new int[] { 1, -5, 96, 13, 16, 104 };
+
+bool isNegativeNumberExists = false;
+for (int index = 0; index < numbers.Length; index++)
+{
+	if (numbers[index] < 0)
+	{
+		isNegativeNumberExists = true;
+		break;
+	}
+}
+
+string status = isNegativeNumberExists ? "var" : "yok";
+Console.WriteLine($"Negatif sayı {status}");

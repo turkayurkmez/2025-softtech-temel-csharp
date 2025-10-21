c25d1eec361559c9c391d5372ff9c6cc5b1d6730
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-21 15:59:51 +0300

    Add C# console app with Product class and demo
    Introduced a new Visual Studio solution (`OOP.sln`) and project file (`ObjectVSClass.csproj`) for a C# console application. Added a `Product` class with `Name` and `Price` fields. Updated `Program.cs` to demonstrate instance creation, property modification, reference equality, and pattern matching with the `Product` class.

---
+++ OOP/ObjectVSClass/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ObjectVSClass;
+
+Console.WriteLine("Hello, World!");
+
+Product product1 = new Product();
+product1.Name = "Gözlük";
+Product product2 = new Product();
+product2.Name = "Kalem";
+
+Product product3 = product1;
+
+
+Console.WriteLine(product3.Name);
+product3.Name = "Defter";
+Console.WriteLine(product1.Name);
+
+Console.WriteLine($"product1 hashCode: ({product1.GetHashCode()}\nproduct3 hashcode: ({product3.GetHashCode()})");
+Console.WriteLine($"product2 hascode: ({product2.GetHashCode()})");
+if (product1 == product3)
+{
+    Console.WriteLine("Aynı referans... Tek instance");
+}
+
+if (product1 != product2)
+{
+    Console.WriteLine("iki farklı instance!");
+}
+//pattern matching:
+if (product1 is Product p)
+{
+    string name = p.Name;
+    Console.WriteLine(name);
+}

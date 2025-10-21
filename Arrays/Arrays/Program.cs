63bf5e832a0a6ead369d4cbace68f030735ed389
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-21 10:01:03 +0300

    Add Arrays and whileLoop projects with examples
    This commit introduces two new projects: "Arrays" and "whileLoop". Each project includes its own solution and project files targeting .NET 9.0, with configurations for output type, implicit usings, and nullable reference types.
    
    The "Arrays" project features a `Program.cs` that demonstrates array usage, including initialization, element access, and converting two-digit numbers to Turkish words, along with examples of ranges and string joining.
    
    The "whileLoop" project contains a `Program.cs` that implements a number guessing game, providing user feedback on guesses and allowing for repeated play.

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+//1. Kapasiteyi biliyorum, değerleri bilmiyorum.
+int[] ages = new int[10];
+ages[0] = 35;
+ages[9] = 24;
+//2. Değerleri biliyorum:
+string[] monthNames = { "Ocak","Şubat","Mart","Nisan","Mayıs" };
+int[] digits = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+Console.WriteLine(monthNames[0]);
+Console.WriteLine(monthNames[monthNames.Length-1]);
+
+Console.WriteLine($"Son ay: {monthNames[^1]}");
+Console.WriteLine($"Son ay: {monthNames[^2]}");
+
+
+Console.WriteLine(string.Join(",", digits[0..3]));
+Console.WriteLine(string.Join(",", digits[3..7]));
+Console.WriteLine(string.Join(",", digits[^3..^0]));
+
+/*
+ *  54
+ *  Elli dört
+ */
+
+string[] onlar = { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+string[] birler = { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+
+Console.WriteLine("İki basamaklı bir sayı girin:");
+string number = Console.ReadLine();
+if (number.Length <=2 )
+{
+    int num = Convert.ToInt32(number);
+    int valueTen = num / 10;
+    int valueOne = num % 10;
+
+    Console.WriteLine(onlar[valueTen] + " " + birler[valueOne]);
+
+}
+
+for (int i =1; i < 100; i++)
+{
+    int valueTen = i / 10;
+    int valueOne = i % 10;
+
+    Console.WriteLine(onlar[valueTen] + " " + birler[valueOne]);
+}
---
+++ Loops/whileLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Random randomNumberGenerator = new Random ();
+string userAnswer = string.Empty;
+do
+{
+    var generatedNumber = randomNumberGenerator.Next(1, 100);
+    var isNumberFinded = false;
+    while (!isNumberFinded)
+    {
+        Console.WriteLine("Sayıyı tahmin edin:");
+        int suggestedNumber = Convert.ToInt32(Console.ReadLine());
+        if (generatedNumber > suggestedNumber)
+        {
+            Console.WriteLine("Yukarı");
+        }
+        else if (generatedNumber < suggestedNumber)
+        {
+            Console.WriteLine("Aşağı");
+        }
+        else
+        {
+            Console.WriteLine("Bildiniz");
+            isNumberFinded = true;
+        }
+    }
+    Console.WriteLine("Tekrar oynamak ister misiniz? (E/H)"); 
+    userAnswer = Console.ReadLine();
+} while (userAnswer == "E");
+

35d01c564a06a04a0aefa47e5c3dd64ddc9816c7
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-22 16:03:13 +0300

    Add .NET solution and project files with sample code
    This commit introduces a Visual Studio solution file (`PatternMatching.sln`) and a project file (`PatternMatching.csproj`) for a .NET application. The project is configured with the necessary settings, including the SDK and target framework. Additionally, the `Program.cs` file implements a console application that creates a list of `Person` records and demonstrates pattern matching to filter and display names of persons older than 18 years.

---
+++ PatternMatching/PatternMatching/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+var person = new Person(8, "Osman", 45, "Eskişehir");
+var person1 = new Person(1, "Ayşe", 45, "Amasya");
+var person2 = new Person(2, "Zeynep", 17, "Sivas");
+
+List<Person> persons = new List<Person>() { person, person1, person2 };
+
+
+foreach (var item in persons)
+{
+    //if (item != null && person.Age > 18 && person.City == "Eskişehir")
+    //{
+    //    Console.WriteLine("Üç önerme && ile birleşti");
+    //}
+
+    //pattern mtching:
+    if (item is { Age: > 18  } p)
+    {
+        Console.WriteLine("Tek bir pattern matching kullanıldı!");
+        Console.WriteLine($"{p.Name}");
+    }
+
+}
+
+
+
+
+//record tanımı:
+public record Person(int Id, string Name, int Age, string City);
+

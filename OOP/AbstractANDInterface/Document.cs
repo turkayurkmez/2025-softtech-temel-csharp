005f8fe092cfd642eb33886bc0c04b847b9d3ee1
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-22 12:32:02 +0300

    Add AbstractANDInterface project with document classes
    Updated the solution to include a new project "AbstractANDInterface" and its project file.
    Created `Document.cs` with an abstract class `Document` and derived classes: `WordDocument`, `ExcelDocument`, and `PDFDocument`, each implementing methods for loading, printing, and saving documents.
    Introduced a `DocumentPrinter` class for printing documents, and updated `Program.cs` to demonstrate the new functionality.

---
+++ OOP/AbstractANDInterface/Document.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractANDInterface
+{
+    public abstract class Document
+    {
+        public void Copy(string from, string to)
+        {
+            Console.WriteLine("Kopyalandı....");
+        }
+
+        public void ChangeTitle(string title)
+        {
+            Console.WriteLine(title);
+        }
+        //Her dokümanın kayıt işlemi farklı olacak!
+        public abstract void Save(string filename);        
+        public abstract void Load(string filename);
+        public abstract void Print();
+
+    }
+
+    public class WordDocument : Document
+    {
+        public override void Load(string filename)
+        {
+            Console.WriteLine("Word yükleniyor");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Word çıktısı.....");
+
+        }
+
+        public override void Save(string filename)
+        {
+            Console.WriteLine("Word kaydediliyor");
+
+        }
+    }
+
+    public class ExcelDocument: Document
+    {
+        public override void Load(string filename)
+        {
+            Console.WriteLine("Excel yükleniyor");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Excel çıktısı.....");
+
+        }
+
+        public override void Save(string filename)
+        {
+            Console.WriteLine("Excel kaydediliyor");
+
+        }
+    }
+
+    public class PDFDocument : Document
+    {
+        public override void Load(string filename)
+        {
+            Console.WriteLine("PDF yükleniyor");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("PDF çıktısı.....");
+
+        }
+
+        public override void Save(string filename)
+        {
+            Console.WriteLine("PDF kaydediliyor");
+
+        }
+    }
+
+    public class DocumentPrinter
+    {
+        public void Print(Document document)
+        {
+            document.Print();
+        }
+    }
+
+}
---
+++ OOP/AbstractANDInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractANDInterface;
+
+Console.WriteLine("Hello, World!");
+PDFDocument pdf = new PDFDocument();
+ExcelDocument excel = new ExcelDocument();
+WordDocument word = new WordDocument();
+
+DocumentPrinter printer = new DocumentPrinter();
+printer.Print(pdf);
+printer.Print(excel);
+printer.Print(word);

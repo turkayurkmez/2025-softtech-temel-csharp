67b964495db277963064fb756c21acfa30ed1392
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-22 10:14:54 +0300

    Add Encapsulation project and Product class
    Updated the solution to include a new "Encapsulation" project with .NET 9.0 SDK. Created `Encapsulation.csproj` for project configuration. Added `Product.cs` defining a `Product` class with properties and methods for price management. Implemented `Program.cs` as the entry point to demonstrate product usage.

---
+++ OOP/Encapsulation/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Product
+    {
+        private decimal price;
+        public Product()
+        {
+            Test = 8;
+        }
+        public void SetPrice(decimal price)
+        {
+            if (price<=0)
+            {
+                throw new ArgumentException("fiyat değeri negatif olamaz!");
+            }
+
+            this.price = price;
+        }
+
+        public decimal GetPrice() { 
+            return this.price;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description { get; set; }
+
+        private int stockCount = 0;
+        public bool IsStock { get { return stockCount > 0; } }
+
+        public int Test { get; private set; }
+
+        public int Test2 { private get; set; }
+
+        public void ChangeTest()
+        {
+            Test = 10;
+            //private set: sadece nesne içinde set edilebilir:
+            //init: sadece object initializer ({}) ile değer atanabilir.
+            //set hiç yok: constructor dışında hiçbir yerden set edilemez
+        }
+
+    }
+}
---
+++ OOP/Encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Encapsulation;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Ürünün fiyatını giriniz:");
+
+decimal price = Convert.ToDecimal(Console.ReadLine());
+//if (price < 0)
+//{
+//    Console.WriteLine("Fiyat negatif olamaz!");
+//    return;
+//}
+
+Product product = new Product();
+product.SetPrice(price);
+product.Name = "Deneme";
+product.Description = "Açıklama";
+
+Product product2 = new Product { Test = 4, Name="Test name", Description="Test Desc" };

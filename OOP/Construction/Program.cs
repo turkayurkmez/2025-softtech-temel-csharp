10ae1241183be73ec995fca286b113bac50bc9bc
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-22 11:09:25 +0300

    Add Construction project and report generation functionality
    - Updated solution file to include new "Construction" project.
    - Created `Construction.csproj` with SDK-style format targeting .NET 9.0.
    - Modified `Program.cs` to demonstrate usage of `ReportGenerator`.
    - Added `ReportGenerator.cs` with report generation logic and enums.

---
+++ OOP/Construction/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Construction;
+
+Console.WriteLine("Hello, World!");
+
+ReportGenerator generator = new ReportGenerator("file.smp", ReportFormatEnums.HTML);
+
+Console.WriteLine(generator.ReportFormat);
+
+ReportGenerator generator1 = new ReportGenerator("C:\\sample.xslx");
+
+Console.WriteLine(generator1.ReportFormat);
+
---
+++ OOP/Construction/ReportGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction
+{
+
+    public class ReportFormatTypes
+    {
+        public const string PDF = "PDF";
+        public const string HTML = "HTML";
+    }
+
+    public enum ReportFormatEnums
+    {
+        PDF,
+        HTML
+    }
+    public class ReportGenerator
+    {
+        //Amaç: Bir excel dosyası içindeki verileri okuyarak, rapor oluşturan nesne.
+
+
+        public ReportFormatEnums ReportFormat { get; set; }
+        public string ExcelFilePath { get; set; }
+
+        //public ReportGenerator() 
+        //{
+        //    ReportFormat = "PDF";
+        //}
+
+        public ReportGenerator(string excelFilePath):this(excelFilePath, ReportFormatEnums.PDF ) 
+        {
+            //ExcelFilePath = excelFilePath;
+            //ReportFormat = "PDF";
+            //bilmemne(excelFilePath, "PDF");
+        }
+
+        public ReportGenerator(string excelFilePath, ReportFormatEnums reportFormat)
+        {
+            ExcelFilePath = excelFilePath;
+            ReportFormat = reportFormat;
+        }
+
+
+
+    }
+}

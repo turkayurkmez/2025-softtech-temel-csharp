a13d8d5b9c6db7ca27659bcf83367452267c1381
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-22 15:15:48 +0300

    Add .NET 9.0 project with Student and ClassRoom classes
    Introduced a new Visual Studio solution and project for a .NET application targeting .NET 9.0.
    Updated `Program.cs` to implement a console app that manages and displays a list of `Student` objects.
    Defined `Student` and `ClassRoom` classes in `Student.cs`, enabling sorting and management of student data.

---
+++ BuiltinInterfaces/BuiltinInterfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuiltinInterfaces;
+
+Console.WriteLine("Hello, World!");
+
+List<Student> students = new ()
+{
+    new(){ Id=1, Name ="Türkay", Score=90},
+    new(){ Id=2, Name ="Berke", Score=75},
+    new(){ Id=3, Name ="Tuğba", Score=100}
+
+
+};
+
+ClassRoom room = new ClassRoom();
+room.AddRange(students);
+room.SortStudents();
+foreach (var item in room)
+{
+    Console.WriteLine($"{item.Name}\t{item.Score}");
+}
---
+++ BuiltinInterfaces/BuiltinInterfaces/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltinInterfaces
+{
+    public class Student : IComparable<Student>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public int CompareTo(Student? other)
+        {
+            if (this.Score > other?.Score)
+            {
+                return 1;
+            }
+            else if (this.Score < other?.Score) { 
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    public class ClassRoom : IEnumerable<Student>
+    {
+        public int Id { get; set; }
+        private List<Student> students = new List<Student>();
+        public void Add(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void AddRange(IEnumerable<Student> studentCollection)
+        {
+            students.AddRange(studentCollection);
+        }
+
+        public void Remove(Student student)
+        {
+            students.Remove(student);
+        }
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+
+        public void SortStudents()
+        {
+            students.Sort();
+           
+        }
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            foreach (var student in students)
+            {
+                yield return student;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

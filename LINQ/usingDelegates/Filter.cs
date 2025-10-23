3dde6ae3e034617374392878c2fa00c1d868ef03
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-23 14:54:42 +0300

    Add usingDelegates project with filtering functionality
    This commit introduces a new project named "usingDelegates" to the solution. It includes a `Filter` class in `Filter.cs` that provides methods for filtering numbers using delegates. The `Program.cs` file demonstrates the usage of these methods with various delegate implementations. Additionally, the project file `usingDelegates.csproj` is created with the necessary SDK and properties for targeting .NET 9.0.

---
+++ LINQ/usingDelegates/Filter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingDelegates
+{
+    public static class Filter
+    {
+       // public delegate bool Criteria(int number);
+       public static int[] FilterNumbers(int[] numbers, Func<int,bool> criteria)
+        {
+            List<int> filteredNumbers = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (criteria(number))
+                {
+                    filteredNumbers.Add(number);
+                }
+            }
+
+            return filteredNumbers.ToArray();
+        }
+
+        public static void OperateWithNumbers(int[] numbers,Action<int[]> action) {
+
+            action(numbers);
+        }
+
+
+    }
+}
---
+++ LINQ/usingDelegates/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using usingDelegates;
+
+Console.WriteLine("Hello, World!");
+var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+bool isEven(int number)
+{
+    return number % 2 == 0;
+}
+
+bool isOdd(int number)
+{
+    return number %2 == 1;
+}
+
+//void showNumbers(int[] numbers)
+//{
+//    Console.WriteLine(string.Join(",",numbers));
+//}
+//v1.0:
+ var evenNumbers = Filter.FilterNumbers(numbers,isEven);
+//v2.0
+var bigThanFive = Filter.FilterNumbers(numbers, delegate (int num) { return num > 5; });
+//v3.5:
+var smallThanFive = Filter.FilterNumbers(numbers, n => n < 5);
+//showNumbers(evenNumbers);
+//showNumbers(bigThanFive);
+//showNumbers(smallThanFive);
+
+Filter.OperateWithNumbers(evenNumbers, nums => Console.WriteLine( string.Join(",", nums)));
+Filter.OperateWithNumbers(smallThanFive, nums => nums.ToList().ForEach(x => Console.WriteLine(x)));
+
+var result = numbers.Where(n => n >= 8)
+                    .ToList();
+Console.WriteLine("--------------------------");
+result.ForEach(x => Console.WriteLine(x));
+                      
+
+

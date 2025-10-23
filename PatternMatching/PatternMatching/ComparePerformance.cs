d1f2a458a5cb5c74fb56fdfb2803481fd12f902c
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-23 11:11:25 +0300

    Enhance Person class and add city analysis features
    Updated the `Person` class to include a `Job` parameter. Introduced the `CityDetails` class for city information and added the `analyzeCity` method for classification using pattern matching. Implemented `getMovementDirection` and `getTemperatureStatus` methods to demonstrate tuple and relational pattern matching. Added `ComparePerformance.cs` for object classification using traditional checks and pattern matching.

---
+++ PatternMatching/PatternMatching/ComparePerformance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatching
+{
+    public static class ComparePerformance
+    {
+        public static string OldwayClassification(object obj)
+        {
+            if (obj == null) return "null değer";
+
+            if(obj is string str)
+            {
+                if (str.Length > 10) return "uzun string";
+                if (str.Length <3 ) return "kısa string";
+                return "normal uzunlukta string";
+            }
+
+            if (obj is int i)
+            {
+                if (i > 100) return "Büyük sayı";
+                if (i < 0) return "Negatif sayı";
+                return "0 ile 100 arası pozitif sayı";
+            }
+
+            return "Bilinmeyen veri tipi";
+        }
+
+        public static string PatternMatchingClassification(object obj)
+        {
+            return obj switch
+            {
+                null => "null değer",
+                string s when s.Length > 10 => "uzun string",
+                string s when s.Length < 3 => "kısa string",
+                string s => "normal uzunlukta string",
+                int i when i>100 => "büyük string",
+                int i when i<0 =>"negatif sayı",
+                int => "0 ile 100 arasında pozitif sayı",
+                _ => "Bilinmeyen veri tipi"
+
+            }; 
+        }
+    }
+}
--- PatternMatching/PatternMatching/Program.cs
+++ PatternMatching/PatternMatching/Program.cs
@@ -4,9 +4,9 @@ Console.WriteLine("Hello, World!");
 
 
 
-var person = new Person(8, "Osman", 45, "Eskişehir");
-var person1 = new Person(1, "Ayşe", 45, "Amasya");
-var person2 = new Person(2, "Zeynep", 17, "Sivas");
+var person = new Person(8, "Osman", 45, "Eskişehir","Mühendis");
+var person1 = new Person(1, "Ayşe", 45, "Amasya","Öğretmen");
+var person2 = new Person(2, "Zeynep", 17, "Sivas","Esnaf");
 
 List<Person> persons = new List<Person>() { person, person1, person2 };
 
@@ -18,8 +18,14 @@ foreach (var item in persons)
     //    Console.WriteLine("Üç önerme && ile birleşti");
     //}
 
+    //if (item is Person)
+    //{
+    //    var pr = (Person)item;
+    //    Console.WriteLine(pr.Name);
+    //}
+
     //pattern mtching:
-    if (item is { Age: > 18  } p)
+    if (item is { Age: >18, City:"Eskişehir" } p)
     {
         Console.WriteLine("Tek bir pattern matching kullanıldı!");
         Console.WriteLine($"{p.Name}");
@@ -28,8 +34,77 @@ foreach (var item in persons)
 }
 
 
+List<CityDetails> cityDetails = new List<CityDetails>()
+{
+    new(1,"Eskişehir","İç Anadolu",800000),
+    new(2,"İstanbul","Marmara",20000000),
+    new(3,"Ankara","İç Anadolu",4900000)
+};
+
+//Property Pattern:
+string analyzeCity(CityDetails cityDetails)
+{
+    return cityDetails switch
+    {
+        { Population: <= 500000 } => "Küçük şehir",
+      //  { Population: < 1000000} => "Normal büyükükte",
+        { Population: > 10000000} => "Kalabalık",
+        { Region: "İç Anadolu", Population: <= 1000000} => "Anadolu'nun kalbinde normal bir şehir",
+        { Population: >= 1000000 and <=5000000 } => $" Potansiyel risk"
+    };
+}
+
+foreach (var item in cityDetails)
+{
+    Console.WriteLine($"{item.Name}:{analyzeCity(item)}");
+}
+
+//Tuple Pattern 
+string getMovementDirection(int deltaX, int deltaY)
+{
+    return (deltaX, deltaY) switch
+    {
+        (0,0) =>"Durgun",
+        (>0,0) =>"Doğu",
+        (<0,0) =>"Batı",
+        (0,>0) =>"Kuzey",
+        (0,<0) =>"Güney",
+        (>0,>0)=>"Kuzeydoğu",
+        (<0,>0) => "Kuzeybatı",
+        (>0,<0) => "Güneydoğu",
+        (<0,<0) =>"GüneyBatı"      
+        
+
+    };
+}
+
+//Relational Pattern:
+string getTemperatureStatus(double celcius)
+{
+    return celcius switch
+    {
+        < 0 => "Donuyor",
+        >= 0 and < 10 => "Çok soğuk",
+        >= 10 and < 20 => "Soğuk",
+        >= 20 and < 30 => "Ilık",
+        double.NaN => "Hatalı ölüm",
+        _ => "Tanımsız değer"
+    };
+}
+
+Console.WriteLine("x:5 y:7 ise " + getMovementDirection(5,7));
+Console.WriteLine("x:0 y:-7 ise " + getMovementDirection(0, -7));
+Console.WriteLine("x:-1 y:0 ise " +  getMovementDirection(-1, 0));
+
+Console.WriteLine($"Hava sıcaklığı: 15 derece -> {getTemperatureStatus(15)}");
+Console.WriteLine($"Hava sıcaklığı: 25 derece -> {getTemperatureStatus(25)}");
+Console.WriteLine($"Hava sıcaklığı: -5 derece -> {getTemperatureStatus(-5)}");
+
+
+
 
 
 //record tanımı:
-public record Person(int Id, string Name, int Age, string City);
+public record Person(int Id, string Name, int Age, string City, string Job);
 
+public record CityDetails(int Id, string Name,string Region, int Population);
